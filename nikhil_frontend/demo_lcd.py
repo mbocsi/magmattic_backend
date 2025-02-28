c0d0beb6b8c44beb4f589c6dc8dfd7f647e9a1c6
nikhilgudladana <155686370+nikhilgudladana@users.noreply.github.com>
2025-02-28 16:27:42 -0600

    Update demo_lcd.py

--- nikhil_frontend/demo_lcd.py
+++ nikhil_frontend/demo_lcd.py
@@ -1,23 +1,31 @@
-
-
-import drivers
+from RPLCD.i2c import CharLCD
 from time import sleep
 
-
-display = drivers.Lcd()
-
+# Initialize the LCD with I2C
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, 
+             cols=16, rows=2, dotsize=8)
 
 try:
     while True:
-        #  sentences = 16 characters long!
+        # Writing to display
         print("Writing to display")
-        display.lcd_display_string("Greetings!", 1)  
-        display.lcd_display_string("Demo Code", 2)  
-        sleep(2)                                           
-        display.lcd_display_string("I am a display!", 1)   
-        sleep(2)                                           
-        display.lcd_clear()                                
-        sleep(2)                                           
+        lcd.clear()
+        lcd.cursor_pos = (0, 0)  # First line, first position
+        lcd.write_string("Greetings!")
+        lcd.cursor_pos = (1, 0)  # Second line, first position
+        lcd.write_string("Demo Code")
+        sleep(2)
+        
+        lcd.clear()
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string("I am a display!")
+        sleep(2)
+        
+        lcd.clear()
+        sleep(2)
 except KeyboardInterrupt:
     print("Cleaning up!")
-    display.lcd_clear()
+    lcd.clear()
+finally:
+    # Make sure to clear the display even if an error occurs
+    lcd.clear()
