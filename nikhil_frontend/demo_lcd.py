caf2a2c8bda83e28aa2ee23bc391ac21a32001d7
nikhilgudladana <155686370+nikhilgudladana@users.noreply.github.com>
2025-02-28 16:25:15 -0600

    Update demo_lcd.py

--- nikhil_frontend/demo_lcd.py
+++ nikhil_frontend/demo_lcd.py
@@ -11,8 +11,8 @@ try:
     while True:
         #  sentences = 16 characters long!
         print("Writing to display")
-        display.lcd_display_string("Greetings Human!", 1)  
-        display.lcd_display_string("Demo Pi Guy code", 2)  
+        display.lcd_display_string("Greetings!", 1)  
+        display.lcd_display_string("Demo Code", 2)  
         sleep(2)                                           
         display.lcd_display_string("I am a display!", 1)   
         sleep(2)                                           
