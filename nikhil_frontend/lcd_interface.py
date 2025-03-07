fd7fceb0c025cff62fd42b2dca3dd32e4053057b
nikhilgudladana <155686370+nikhilgudladana@users.noreply.github.com>
2025-03-07 07:13:30 -0600

    Update lcd_interface.py

--- nikhil_frontend/lcd_interface.py
+++ nikhil_frontend/lcd_interface.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app_interface import AppComponent
 
 class LCDInterface(AppComponent):
