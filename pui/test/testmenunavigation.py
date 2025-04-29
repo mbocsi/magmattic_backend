e5584659960dc50480ab23a8ce2d6bca32d40c01
nikhilgudladana <155686370+nikhilgudladana@users.noreply.github.com>
2025-04-29 15:51:49 -0500

    Update testmenunavigation.py

--- pui/test/testmenunavigation.py
+++ pui/test/testmenunavigation.py
@@ -6,7 +6,7 @@ import sys
 import os
 import time
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from nikhil_frontend import LCDController
+from pui import PUIComponent
 import RPi.GPIO as GPIO
 
 # Configure logging
